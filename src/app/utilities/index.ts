09097427dcebdd312f00788efbf8f4fcb7682854
savaryt <thibaultsavary@protonmail.ch>
2018-08-17 09:57:40 +0200

    missing semi

--- src/app/utilities/index.ts
+++ src/app/utilities/index.ts
@@ -2,5 +2,5 @@ export function getDbId(storeId: string) {
   return storeId.split('-')[0];
 }
 export function getStoreId(dbId: string, size: string) {
-  return `${dbId}-${size}`
+  return `${dbId}-${size}`;
 }
